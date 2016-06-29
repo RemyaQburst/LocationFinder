6a8e02b9e9877a4d57ce305c67f10b1b1aa88dbf
Remya <remyaj@qburst.com>
2016-06-29 10:52:23 +0530

    removing angular # tag

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,13 +1,17 @@
 (function () {
     'use strict';
 
-   var Config = function ($translateProvider) {
+   var Config = function ($translateProvider,$locationProvider) {
    		$translateProvider.useStaticFilesLoader({
    			prefix: 'app/lang/',
    			suffix: '.json'
    		});
    		$translateProvider.preferredLanguage('en');
    		$translateProvider.fallbackLanguage(['en', 'fr']);
+       $locationProvider.html5Mode({
+          enabled: true,
+          requireBase: false
+        });
    };
    angular.module('locationApp')
     .config(Config);
